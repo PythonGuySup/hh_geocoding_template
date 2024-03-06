949d0c98a2431da5f60624764c2e3782c95ed3b4
PythonGuySup <egorkachanov2006@gmail.com>
2024-03-07 01:55:49 +0300

    Initial commit

---
+++ work_time_analysis.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+def dict_to_dataframe(data_dict):
+    """Преобразования словаря с временем работы алгоритмов в pandas DataFrame."""
+    data = []
+    for alg, times in data_dict.items():
+        for size, time in times.items():
+            data.append({'Алгоритм': alg, 'Количество данных': size, 'Время работы (мс)': time})
+    # Создание DataFrame
+    df = pd.DataFrame(data)
+    return df
+
+
+def plot_data(df):
+    """Строит график времени работы алгоритмов на основе DataFrame, упорядоченный по количеству данных."""
+    fig, ax = plt.subplots()
+    # Сортировка по алгоритму и количеству данных для последовательного отображения
+    for algorithm in df['Алгоритм'].unique():
+        alg_data = df[df['Алгоритм'] == algorithm]
+        ax.plot(alg_data['Количество данных'], alg_data['Время работы (мс)'], label=algorithm, marker='o')
+    ax.set_xlabel('Количество данных')
+    ax.set_ylabel('Время работы (мс)')
+    ax.legend()
+    plt.show()
+
