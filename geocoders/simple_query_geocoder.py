a7018c129aedc28825bb27925579bed51522569f
PythonGuySup <egorkachanov2006@gmail.com>
2024-03-10 23:19:41 +0300

    Незначительное исправление

--- geocoders/simple_query_geocoder.py
+++ geocoders/simple_query_geocoder.py
@@ -6,7 +6,7 @@ from api import API
 class SimpleQueryGeocoder(Geocoder):
     def _apply_geocoding(self, area_id: str) -> str:
         node = API.get_area(area_id)
-        full_address = "" + node.name
+        full_address = node.name
 
         while node.parent_id:
             node = API.get_area(node.parent_id)
